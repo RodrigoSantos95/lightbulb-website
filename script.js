f631d89ac991d663615fe581344bf64f6ed2c99e
Rodrigo Costa Santos <rodrigoscsantos@live.com>
2023-01-24 15:35:19 -0300

    interaction feature

---
+++ script.js
@@ -0,0 +1,11 @@
+function acender() {
+    document.querySelector('#luz').src='pictures/lampada-acesa.jpg';
+}
+
+function apagar() {
+    document.querySelector('#luz').src='pictures/lampada-apagada.jpg';
+}
+
+function quebrar() {
+    document.querySelector('#luz').src='pictures/lampada-quebrada.jpg'
+}
